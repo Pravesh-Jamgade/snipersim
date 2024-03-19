7ff1c225ad0a2dba0e6be969d612356302bb57b9
Pravesh-Jamgade <praveshjamgade@gmail.com>
2024-03-19 12:08:54 +0530

    simplified replaced by array

--- common/plugins/mem_level_info.h
+++ common/plugins/mem_level_info.h
@@ -20,7 +20,7 @@ class MemDataLogger
     // indexing: 1+1+(2)=4, 1+2+(2)=5, 1+3+(2)=6 [oa reaplced by oa,ea,pa] 
     // indexing: 2+1+(4)=7, 2+2+(4)=8, 2+3+(4)=9 [ea reaplced by oa,ea,pa] 
     // indexing: 3+1+(6)=10, 3+2+(6)=11, 3+3+(6)=12 [pa replaced by oa,ea,pa] 
-    IntPtr* replaced_by;    
+    IntPtr replaced_by[4][4] = {0};    
     String name;
     int coreid;
     static bool once_a;
@@ -34,7 +34,6 @@ class MemDataLogger
         type_miss_arr = (IntPtr*)calloc(ARRAY_TYPE_SIZE, sizeof(IntPtr));
         type_hit_arr = (IntPtr*)calloc(ARRAY_TYPE_SIZE, sizeof(IntPtr));
 
-        replaced_by = (IntPtr*)calloc(13, sizeof(IntPtr));
     }
 
     MemDataLogger(int coreid, String name)
@@ -45,8 +44,6 @@ class MemDataLogger
         type_access_arr = (IntPtr*)calloc(ARRAY_TYPE_SIZE, sizeof(IntPtr));
         type_miss_arr = (IntPtr*)calloc(ARRAY_TYPE_SIZE, sizeof(IntPtr));
         type_hit_arr = (IntPtr*)calloc(ARRAY_TYPE_SIZE, sizeof(IntPtr));
-
-        replaced_by = (IntPtr*)calloc(13, sizeof(IntPtr));
     }
 
     void add_access(int type){type_access_arr[type]++;}
@@ -71,13 +68,21 @@ class MemDataLogger
         {
             _LOG_CUSTOM_LOGGER(Log::Warning, Log::LogCustom::A, "%s,%ld,%ld,%ld,%s\n", get_arr_name(i).c_str(), type_access_arr[i], type_hit_arr[i], this->coreid, this->name.c_str());
         }
-
+        
+        _LOG_CUSTOM_LOGGER(Log::Warning, Log::LogCustom::B, "----------------------------------------------------------------\n");
         String s= "";
-        for(int i=1; i< 13; i++)
+        for(int i=1; i< 4; i++)
         {
-            s = s + String(std::to_string(replaced_by[i]).c_str()) + ",";
+            String row = "";
+            for(int j=0; j< 4; j++)
+            {
+                row = row + String(std::to_string(replaced_by[i][j]).c_str()) + ",";
+            }
+            s = row + '\n';
         }
         _LOG_CUSTOM_LOGGER(Log::Warning, Log::LogCustom::B, "%s %ld, %s\n", s.c_str(), this->coreid, this->name.c_str());
+        _LOG_CUSTOM_LOGGER(Log::Warning, Log::LogCustom::B, "----------------------------------------------------------------\n");
+
     }
 
     /*
@@ -86,14 +91,7 @@ class MemDataLogger
     */
     void replacing(int new_type, int old_type)
     {
-        int index = new_type + old_type;
-        // add delta +0 NONE , +2 OFFSET/INDEX ARRAY, +4 EDGE/NEIGH/STRUCTURAL ARRAY, +6 PROPERTY ARRAY, 
-        if(old_type == 0) index += 2;
-        else if(old_type == 1) index += 4;
-        else if(old_type == 2) index += 6;
-
-        if(index >= 1 && index < 13)
-            replaced_by[index]++;
+        replaced_by[new_type][old_type]++;
     }   
 
 };
